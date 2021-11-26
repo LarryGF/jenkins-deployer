b4d6e4e28aa3dd48f16c86e751627eb7c6a6a44e
LarryGF <larrywtf609@gmail.com>
2021-11-26 15:07:10 +0000

    Added run.sh to start the deployment

---
+++ README.md
@@ -0,0 +1,5 @@
+# Running for the first time
+
+```bash
+    bash run.sh
+```
--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
 #! /bin/bash
+echo 'Trying to figure out GID of docker.sock'
+DOCKER_GID=`ls -ldn ${DOCKER_PATH:-'/var/run/docker.sock'} | awk '{print $4}'`
+echo "Docker GID: ${DOCKER_GID}"
+echo  "Bringing the compose up with correct ownership for docker.sock"
+DOCKER_GID=$DOCKER_GID docker-compose up --build
