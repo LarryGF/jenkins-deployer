42e52d04f352cfc71b4c00fe9a06114abf49908b
LarryGF <larrywtf609@gmail.com>
2021-11-27 05:16:40 +0000

    Several changes
    - Updated plugin list
    - Added more tools
    - Tweaked some system/plugins settings
    - Created Vault container (not working, needs tweaking)
    - Created location in nginx for vault (still needs tweaking)

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ echo 'Trying to figure out GID of docker.sock'
 DOCKER_GID=`ls -ldn ${DOCKER_PATH:-'/var/run/docker.sock'} | awk '{print $4}'`
 echo "Docker GID: ${DOCKER_GID}"
 echo  "Bringing the compose up with correct ownership for docker.sock"
-DOCKER_GID=$DOCKER_GID docker-compose up --build
+DOCKER_GID=$DOCKER_GID docker-compose up --build --remove-orphans
