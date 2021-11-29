7020abf89a62dc1f25ba32ae143678815aac6a4f
LarryGF <larrywtf609@gmail.com>
2021-11-29 00:50:28 +0000

    Changes:
    - Automatically creating admin user in vault

--- run.sh
+++ run.sh
@@ -1,46 +1,54 @@
 #! /bin/bash
-printf "\nTrying to figure out GID of docker.sock"
+#### DOCKER SETUP ###############################################################################
+printf "\n[*] Trying to figure out GID of docker.sock"
 #Need to set the DOCKER_PATH env var before running the script if it's not running in this path
 docker_gid=`ls -ldn ${DOCKER_PATH:-'/var/run/docker.sock'} | awk '{print $4}'`
-printf "\nDocker GID: $docker_gid \n Bringing the compose up with correct ownership for docker.sock"
+printf "\n[*] Docker GID: $docker_gid \n Bringing the compose up with correct ownership for docker.sock"
 
 DOCKER_GID=$docker_gid docker-compose up --build --remove-orphans -d
 
-#Need to set the VAULT_URL env var before running the script if it's running in a different host/port
+#### VAULT SETUP #################################################################################
+
+### INITIAL VARIABLES ##############################################
 SLEEP_BETWEEN_RETRIES_SEC=${SLEEP_BETWEEN_RETRIES_SEC:-'2'}
 MAX_RETRIES=${MAX_RETRIES:-'10'}
+#Need to set the VAULT_URL env var before running the script if it's running in a different host/port
 vault_addr=${VAULT_URL:-'http://127.0.0.1:8200'}
 vault_health_url="${vault_addr}/v1/sys/health"
-export VAULT_ADDR=$vault_addr
-keys_file=${KEYS_FILE:-'./keys/keys'}
+keys_path=${KEYS_PATH:-'./keys/'}
 total_key=${TOTAL_KEYS:-6}
 used_keys=${USED_KEYS:-3}
 
-printf "\nTrying to initialize Vault deployment running on: $vault_addr \nVerifying status of Vault deployment running on: $vault_health_url"
+vault_user=${VAULT_USER:-'admin'}
+vault_pass=${VAULT_PASS:-'admin'}
+#############################################################
+
+printf "\n[*] Trying to initialize Vault deployment running on: $vault_addr \nVerifying status of Vault deployment running on: $vault_health_url"
 for (( i=1; i<="$MAX_RETRIES"; i++ )); do
     response=$(curl --show-error --location --insecure --silent --write-out "HTTPSTATUS:%{http_code}" "$vault_health_url" || true)
     status=$(echo "$response" | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
     if [[ "$status" -eq 200 ]]; then
-        printf "\nVault server $server_ip is initialized, unsealed, and active."
+        printf "\n[**] Vault server $server_ip is initialized, unsealed, and active."
         break
         
         elif [[ "$status" -eq 429 ]]; then
-        printf "\nVault server $server_ip is unsealed and in standby mode."
+        printf "\n[**] Vault server $server_ip is unsealed and in standby mode."
         break
         
         elif [[ "$status" -eq 501 ]]; then
-        printf "\nVault server $server_ip is uninitialized.\n"
-        vault operator init -n $total_key -t $used_keys > output
-        vault_token=$(grep 'Initial Root Token:' output | awk '{print substr($NF, 1, length($NF)-1)}')
-        export VAULT_TOKEN=$vault_token
-        cat output | grep Unseal > $keys_file
-        sed  -i 's/Unseal.*: //g' $keys_file
-        rm output
+        printf "\n[**] Vault server $server_ip is uninitialized."
+        vault operator  init -address=$vault_addr -n $total_key -t $used_keys > output
+        vault_token=$(grep 'Initial Root Token:' output | awk '{print substr($NF, 1, length($NF))}')
+        echo $vault_token > "${keys_path}token"
+        # export VAULT_TOKEN=$vault_token
+        cat output | grep Unseal > "${keys_path}key"
+        sed  -i 's/Unseal.*: //g' "${keys_path}key"
         sleep "$SLEEP_BETWEEN_RETRIES_SEC"
-        printf "Vault server $server_ip has been initialized."
+        printf "\n[**] Vault server $server_ip has been initialized."
+        rm output
         elif [[ "$status" -eq 503 ]]; then
-        printf "\nVault server $server_ip is sealed.\n"
-        head -$used_keys $keys_file | xargs -L1 vault operator unseal
+        printf "\n[**] Vault server $server_ip is sealed.\n"
+        head -$used_keys "${keys_path}key" | xargs -L1 vault operator  unseal -address=$vault_addr
         sleep "$SLEEP_BETWEEN_RETRIES_SEC"
         
         
@@ -49,3 +57,14 @@ for (( i=1; i<="$MAX_RETRIES"; i++ )); do
         sleep "$SLEEP_BETWEEN_RETRIES_SEC"
     fi
 done
+
+printf "\n[*] Initializing auth process and user setup\n"
+vault login -address=$vault_addr token=$vault_token
+printf "\n[*] Creating user/password\n"
+vault auth enable  -address=$vault_addr userpass
+vault policy write -address=$vault_addr $vault_user ./consul/config/admin.hcl
+vault write -address=$vault_addr auth/userpass/users/webui password=$vault_pass policies=$vault_user
+
+# ## CREATE BACKUP TOKEN
+# printf "\n[*] Create backup token..."
+# vault token-create -address=$vault_addr -display-name="backup_token" | awk '/token/{i++}i==2' | awk '{print "backup_token: " $2}' >> "${keys_path}key_backup"
